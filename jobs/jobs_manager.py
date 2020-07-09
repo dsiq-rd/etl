3e55e8f56079f3984c1d03a042316feea66a8d13
dsiq-rd <rupesh.d@digitalshelf.com>
2020-07-09 23:48:00 +0530

    MWS Object Done

--- jobs/jobs_manager.py
+++ jobs/jobs_manager.py
@@ -5,6 +5,7 @@ from schemas.models import ClientMain, Channel, ChannelAccess
 from datetime import datetime
 from jobs.mws_api import mwsRequestMaker
 import config
+import pandas as pd
 
 Base = declarative_base()
 
@@ -24,15 +25,46 @@ class clientManager:
 		return Clients
 
 	def getClientDetails():
+
 		#Get all Client Details
 		Clients = clientManager.clientCollator()
-		clientData = {}
+		clientAccess = {}
+		mwsObject = {}
+		
 		for client in Clients:
+
 			engine = db(environment.get('postgres')+ '/client' + str(client.id))
 			Session = sessionmaker(bind=engine)
 			session = Session()
-			cData = (session.query(Channel).join(ChannelAccess,ChannelAccess.channel_id == Channel.id).all())
-			session.close()			
-			
-			for c in cData: 
-				print(c.__dict__)
+
+			#Channel Access information collected
+			access = session.query(ChannelAccess).all()
+			session.close()
+			alist = []
+			channels = []
+			# for all clients that have channel access setup
+			# Creating a list of unique channel ids
+			if access:
+				mwsObject['client'+str(client.id)] = []
+				for a in access:
+					
+					channels = session.query(Channel).filter(Channel.id == a.channel_id).first()
+					session.close()
+
+					mwsObject['client'+str(client.id)].append(
+						{
+						a.channel_id:
+							{
+							'access_key': channels.channel_AWS_client_id,
+							'secret_key': channels.channel_aws_secret_key,
+							'account_id': a.channel_merchant_id,
+							'mws_auth_token': a.channel_auth_token,
+							'region': channels.channel_country_code,
+							'sqsQueueUrl': a.sqs_endpoint,
+							'developer_id': channels.channel_developer_id,
+							'marketplace_id': channels.channel_marketplace_id
+							}
+						}
+					)
+		print(mwsObject)	
+		return mwsObject
